4f01a79fe48ed60a82b13ad8d7f8f79b0dea183c
Isaac_Santos <isaacdsantos@hotmail.com>
2024-11-26 17:34:04 -0300

    Atualizado arquivo buscas com grafo

--- Python/buscas.py
+++ Python/buscas.py
@@ -5,3 +5,15 @@ class Vertex:
         self.value = value
         self.neighbors = []
 
+# o Grafo adicionara os vertices, arestas e ajudará na buscas dos algoritmo
+
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, label, value=None):
+        self.vertices[label] = Vertex(label, value)
+
+    def add_edge(self, u, v):
+        self.vertices[u].neighbors.append(v)
+        self.vertices[v].neighbors.append(u)  # Para grafo não direcionado
